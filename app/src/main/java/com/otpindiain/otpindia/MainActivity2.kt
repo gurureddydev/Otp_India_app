218fe2bb7e85a888429f6406d06b9fa4457a2954
gurureddydev <guruprasadreddy2000@gmail.com>
2024-03-06 08:10:40 +0530

    removed code

--- app/src/main/java/com/otpindiain/otpindia/MainActivity2.kt
+++ app/src/main/java/com/otpindiain/otpindia/MainActivity2.kt
@@ -8,10 +8,5 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra("Key","My_data")
-        startActivity(intent)
-
     }
 }
